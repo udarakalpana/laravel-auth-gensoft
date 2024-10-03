6faf19deb578fd282c5e74fcae509e500311c0d3
kalpana <kalpanaudara058@gmail.com>
2024-10-03 08:10:05 +0530

    Fetch question and answers pass them into the blade.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -11,6 +12,12 @@ class AdminController extends Controller
 {
     public function showDashboard(): View|Factory|Application
     {
-        return view('admin.dashboard');
+        // lazy loading
+//        $questions = Question::all();
+
+        // eager loading
+        $questions = Question::with('answers')->get();
+
+        return view('admin.dashboard')->with(['questions' => $questions]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -39,26 +39,21 @@
                             </tr>
                             </thead>
                             <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
-                                </th>
-                                <td class="px-6 py-4">
-                                    Silver
-                                </td>
-                                <td class="px-6 py-4">
-                                    Laptop
-                                </td>
-                                <td class="px-6 py-4">
-                                    $2999
-                                </td>
-                                <td class="px-6 py-4">
-                                    $2999
-                                </td>
-                                <td class="px-6 py-4">
-                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                </td>
-                            </tr>
+                            @foreach($questions as $question)
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{$question->question}}
+                                    </th>
+                                    @foreach($question->answers as $answer)
+                                        <td class="px-6 py-4">
+                                            {{$answer->answer}}
+                                        </td>
+                                    @endforeach
+                                    <td class="px-6 py-4">
+                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
