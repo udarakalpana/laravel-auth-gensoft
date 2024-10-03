05e512dd396f4b55b042ccbf4893934f488d2d1b
kalpana <kalpanaudara058@gmail.com>
2024-10-03 07:09:48 +0530

    create question storing part

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Question\QuestionRequest;
+use App\Models\Answer;
+use App\Models\Question;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -14,8 +17,34 @@ class QuestionController extends Controller
         return view('admin.question.create');
     }
 
-    public function storeQuestion(Request $request)
+    public function storeQuestion(QuestionRequest $request)
     {
-       dd($request->all());
+       $validatedQuestionRequest = $request->validated();
+
+       $question = Question::create([
+           'question' => $validatedQuestionRequest['question'],
+           'correct_answer' => $validatedQuestionRequest['correct'],
+       ]);
+
+       if (!$question) {
+           return redirect()->route('dashboard');
+       }
+
+        $answers = [
+            'answer1' => $validatedQuestionRequest['answer1'],
+            'answer2' => $validatedQuestionRequest['answer2'],
+            'answer3' => $validatedQuestionRequest['answer3'],
+            'answer4' => $validatedQuestionRequest['answer4'],
+        ];
+
+        foreach ($answers as $answer) {
+           Answer::create([
+               'question_id' => $question->id,
+               'answer' => $answer
+           ]);
+       }
+
+        return redirect()->route('dashboard');
+
     }
 }
--- app/Http/Requests/Question/QuestionRequest.php
+++ app/Http/Requests/Question/QuestionRequest.php
@@ -6,12 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class QuestionRequest extends FormRequest
 {
+    private array $questionValidationRules =  ['required', 'string', 'min:1'];
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,12 @@ class QuestionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'question' => $this->questionValidationRules,
+            'correct' => $this->questionValidationRules,
+            "answer1" => $this->questionValidationRules,
+            "answer2" => $this->questionValidationRules,
+            "answer3" => $this->questionValidationRules,
+            "answer4" => $this->questionValidationRules
         ];
     }
 }
