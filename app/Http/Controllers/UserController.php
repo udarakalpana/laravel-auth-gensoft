e8adb6352393bc847487882e76b907be9b383f42
kalpana <kalpanaudara058@gmail.com>
2024-10-08 08:53:50 +0530

    Create user answer storing part.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Models\UserAnswer;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -17,8 +19,28 @@ class UserController extends Controller
         return view('user.dashboard')->with('questions', $questions);
     }
 
-    public function answerForQuestion(string $questionId, Request $request)
+    public function answerForQuestion(string $questionId, Request $request): RedirectResponse
     {
-        dd($questionId, $request->all());
+       $validatedAnswerRequest = $request->validate([
+           'answer' => ['required', 'string']
+       ]);
+
+       $user = $request->user();
+
+       $question = Question::findOrFail($questionId);
+
+       $isCorrect = trim($validatedAnswerRequest['answer']) === trim($question->correct_answer);
+
+       UserAnswer::create([
+           'user_id' => $user->id,
+           'question_id' => $question->id,
+           'answer' => $validatedAnswerRequest['answer'],
+           'is_correct' => $isCorrect,
+       ]);
+
+       $message = $isCorrect ? 'Your answer is correct' : 'Your answer is wrong';
+
+
+        return redirect()->route('user-dashboard')->with('message', $message);
     }
 }
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -7,9 +7,19 @@
 
     <div class="max-w-7xl mx-auto mt-4 sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            @if(session('message'))
+
+                <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                    </svg>
+                    <span class="sr-only">Info</span>
+                    <div>
+                        {{session('message')}}
+                    </div>
+                </div>
+            @endif
             <div class="grid grid-cols-2 p-6 text-gray-900 dark:text-gray-100">
-
-
                 @foreach($questions as $question)
                     <div
                         class="max-w-sm p-6 ml-4 mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
